ca4990e9287a104232638f7e4b34b21074cfce77
operando <okanoshinobu8@gmail.com>
2016-08-25 09:08:35 +0900

    🐱

--- app/src/main/java/com/os/operando/neko/launcher/MainActivity.java
+++ app/src/main/java/com/os/operando/neko/launcher/MainActivity.java
@@ -1,10 +1,10 @@
 package com.os.operando.neko.launcher;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
